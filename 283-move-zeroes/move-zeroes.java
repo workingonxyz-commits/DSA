5b5f0ed441c248d7491dc5853ead7b241134fbef
Anant Kumar Bhaskar <workingonxyz@gmail.com>
2026-08-12 20:54:54 +0530

    Time: 2 ms (92.05%) | Memory: 47.8 MB (57.63%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int a=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[a]=nums[i];
+                a++;
+            }
+            
+        }
+        while(a!=nums.length){
+                nums[a]=0;
+                a++;
+            }
+    }
+}
