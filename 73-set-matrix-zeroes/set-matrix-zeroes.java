d6f8ba4fc48bfe947c2efb06de9869aefec7ea53
Anant Kumar Bhaskar <workingonxyz@gmail.com>
2026-08-27 20:39:52 +0530

    Time: 3 ms (16.79%) | Memory: 47.5 MB (80.65%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        HashSet<Integer> map1=new HashSet<>();
+        HashSet<Integer> map2=new HashSet<>();
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    map1.add(i);
+                    map2.add(j);
+                }
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if((map1.contains(i)) || (map2.contains(j))){
+                   matrix[i][j]=0;
+                }
+            }
+        }
+        
+        //return matrix;
+
+    }
+}
