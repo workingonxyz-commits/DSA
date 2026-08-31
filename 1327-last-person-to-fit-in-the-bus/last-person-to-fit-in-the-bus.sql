a59c5ebc9db5000b9bc663bbee895f3b61a66f59
Anant Kumar Bhaskar <workingonxyz@gmail.com>
2026-09-01 02:55:51 +0530

    Time: 842 ms (72.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT person_name
+FROM (
+    SELECT
+        person_name,
+        SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue
+) q
+WHERE total_weight <= 1000
+ORDER BY total_weight DESC
+LIMIT 1;
