23f2c894d9089307a63854c4daf56b1a64c5f63d
Anant Kumar Bhaskar <workingonxyz@gmail.com>
2026-09-14 15:30:39 +0530

    Time: 357 ms (96.38%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
-SELECT e.NAME AS EMPLOYEE FROM EMPLOYEE e INNER JOIN EMPLOYEE f ON e.managerId=f.id where e.salary>f.salary; 
+#SELECT e.NAME AS EMPLOYEE FROM EMPLOYEE e INNER JOIN EMPLOYEE f ON e.managerId=f.id where e.salary>f.salary; 
 #SELECT * FROM EMPLOYEE e INNER JOIN EMPLOYEE f ON e.managerId=f.id
+SELECT e.NAME AS Employee  FROM EMPLOYEE e JOIN EMPLOYEE f ON e.MANAGERID=f.ID 
+WHERE e.SALARY>f.SALARY;
