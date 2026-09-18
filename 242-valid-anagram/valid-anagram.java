444fe6b0f69536ad56cf9136db439105c0390163
Anant Kumar Bhaskar <workingonxyz@gmail.com>
2026-09-19 00:45:24 +0530

    Time: 4 ms (83.68%) | Memory: 44.2 MB (89.31%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] freq=new int[26];
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            freq[ch-'a']++;
+       int[] freq=new int[26];
+       if(s.length()!=t.length()){
+        return false;
+       }
+       for(int i=0;i<s.length();i++){
+        char ch=s.charAt(i);
+        freq[ch-'a']++;
+       } 
+       int[] freq2=new int[26];
+       for(int i=0;i<t.length();i++){
+        char ch=t.charAt(i);
+        freq2[ch-'a']++;
+       }
+       for(int i=0;i<26;i++){
+        if(freq[i]!=freq2[i]){
+            return false;
         }
-        int[] freq2=new int[26];
-        for(int j=0;j<t.length();j++){
-            char ch2=t.charAt(j);
-            freq2[ch2-'a']++;
-        }
-        for(int k=0;k<freq.length;k++){
-            if(freq[k]!=freq2[k]){
-                return false;
-            }
-        }
-        return true;
+       }
+       return true;
     }
 }
