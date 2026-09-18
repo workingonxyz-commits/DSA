e67129f091b0a04f2ee79140075f4b0665ec223b
Anant Kumar Bhaskar <workingonxyz@gmail.com>
2026-09-18 12:36:25 +0530

    Time: 2 ms (57.46%) | Memory: 43.5 MB (7.24%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -4,14 +4,16 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(nums[i])){
                 map.put(nums[i],map.get(nums[i])+1);
-            }else{map.put(nums[i],1);}
+            }
+            else{map.put(nums[i],1);}
         }
+        
         int sum=0;
         for(Map.Entry<Integer,Integer> entry:map.entrySet()){
             if(entry.getValue()==1){
-                sum+=entry.getKey();
+                sum=sum+entry.getKey();
             }
         }
-        return sum;
-    }
+    return sum;
+}
 }
